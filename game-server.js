42323a09eeeb9a3dc67f8dc7bd8fd0088c530710
Cem <commention@gmail.com>
2020-08-27 21:35:59 +0300

    game server

---
+++ game-server.js
@@ -0,0 +1,70 @@
+const WebSocket = require('ws');
+const { createWebSocketStream } = require('ws');
+ 
+const WebSocketServer = new WebSocket.Server({ port: 8080 });
+
+const server = {
+    players : []
+};
+
+WebSocketServer.on('connection', function(socket, ws){
+    console.log('A user connected!');
+
+    var id = Math.floor(Math.random() * 10000000);
+
+    socket.playerId = id;
+    socket.position = { x : 0, y : 0, z : 0 };
+    server.players.push(socket);
+
+    var ipAddress = ws.headers['x-forwarded-for'] || ws.connection.remoteAddress;
+    console.log(ipAddress);
+    
+    socket.on('close', function(data) {
+        server.players.splice(server.players.indexOf(socket), 1);
+
+        server.players.forEach(p => {
+            p.send(JSON.stringify({ action : 'player-left', pp : server.players.length }));
+        });
+    });
+
+    socket.on('message', function(result) {
+        var res = JSON.parse(result);
+
+        if(res.action === 'player-connected') {
+            server.players.forEach(p => {
+                p.send(JSON.stringify({ 
+                    action   : 'player-joined', 
+                    pp       : server.players.length, 
+                    playerId : socket.playerId 
+                }));
+            });
+
+            //send all current players
+
+            for(var playerIndex in server.players){
+                var player = server.players[playerIndex];
+
+                socket.send(JSON.stringify({ 
+                    action   : 'player-joined', 
+                    pp       : server.players.length, 
+                    playerId : player.playerId 
+                }));
+            }
+        }
+
+        if(res.action === 'player-location') {
+            // server.players.forEach(p => {
+            //     server.positions[p.id] = res.position;
+            // });
+            socket.position = res.position;
+
+            server.players.forEach(p => {
+                p.send(JSON.stringify({ 
+                    action   : 'position', 
+                    playerId : socket.playerId,
+                    position : socket.position
+                }));
+            });
+        }
+    });
+});
